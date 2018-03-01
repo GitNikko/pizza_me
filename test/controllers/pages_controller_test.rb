a0d0402974bc983824545798ab69c304d51f7bc8
GitNikko <nic.hunt86@gmail.com>
2018-03-01 20:35:31 +1100

    make username and password validations

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,22 +2,22 @@ require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get pages_home_url
+    get root_path
     assert_response :success
   end
 
   test "should get news" do
-    get pages_news_url
+    get news_path
     assert_response :success
   end
 
   test "should get pizza" do
-    get pages_pizza_url
+    get pizza_path
     assert_response :success
   end
 
   test "should get about" do
-    get pages_about_url
+    get about_path
     assert_response :success
   end
 
