472853766740ac0b4c53237c4b5e40d16715f4e3
GitNikko <nic.hunt86@gmail.com>
2018-03-03 13:21:02 +1100

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    #This object is required to provide the @user argument in the form_for
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Sign up successful!"
+      redirect_to '/'
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    #This limits the data that will get submitted through the params hash.
+    #Permitting only the listed attributes belonging to the :user hash inside params.  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/pizza', to: 'pages#pizza'
   get '/about', to: 'pages#about'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
 end
