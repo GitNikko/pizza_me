adeda6707d1461bce6e96f40d0ebdefb9288a3e1
GitNikko <nic.hunt86@gmail.com>
2018-04-20 10:22:04 +1000

    added redirect for omniauth failure

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,3 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET']
 end
+
+OmniAuth.config.on_failure = -> (env) do
+  Rack::Response.new(['302 Moved'], 302, 'Location' => env['omniauth.origin'] || "/").finish
+end
