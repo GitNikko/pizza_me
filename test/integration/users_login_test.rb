3c9c8145a0355cbf4a5457b2d06c2c5c05609d87
GitNikko <nic.hunt86@gmail.com>
2018-05-08 19:26:37 +1000

    updated edit_users view

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -60,7 +60,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", signup_path, count: 0
     assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", edit_user_path(session[:user_id]), count: 0
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
